f4fe5083058afb4fa45505511c8012db5bb742e2
Kevin Ren <kevin@Kevins-MacBook-Pro.local>
2019-02-27 17:06:38 -0500

    added background image and set up background positions in game

--- Project 3 - Animation Game/Project 3 - Animation Game/GameScene.swift
+++ Project 3 - Animation Game/Project 3 - Animation Game/GameScene.swift
@@ -40,6 +40,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     var grids = true
     
+    var bg = SKSpriteNode(imageNamed: "bg")
     var hoop = SKSpriteNode(imageNamed: "hoop")
     var basketball = SKSpriteNode(imageNamed: "ball")
     
@@ -49,14 +50,61 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     var startGround = SKShapeNode() // Where the balls starts from
     var endGround = SKShapeNode() //Where the ball will stop/bounce
     
+    var pi = Double.pi
+    var wind = CGFloat()
+
     
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         
         if UIDevice.current.userInterfaceIdiom == .phone{
+            GameConstants.gravity = -6
+            GameConstants.YVel = self.frame.height / 4
+            GameConstants.AirTime = 2
+        }
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: GameConstants.gravity)
+        
+        runGame()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
             
+            if GameState.current == .playing && ball.contains(location){
+                    TouchPoints.start = location
+            }
         }
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            if GameState.current == .playing && !ball.contains(location){
+                    TouchPoints.end = location
+                    shootBall()
+            }
+        }
+    }
     
+    func runGame(){
+        GameState.current = .playing
+        
+        let bgScale = CGFloat(bg.frame.width / bg.frame.height)
+        
+        bg.size.height = self.frame.height
+        bg.size.width = bg.size.height * bgScale
+        bg.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        bg.zPosition = 0
+        
+        self.addChild(bg)
+        
+        
+    }
+    
+    func shootBall(){
+        
+    }
 }
